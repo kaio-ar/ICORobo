a20ff45187a3f8370ee4297f4f923876b52a7c11
Alan <alan@ifsc>
2026-05-16 16:49:31 -0300

    Primeira alteração

---
+++ RoboAlan.java
@@ -0,0 +1,12 @@
+package Robocode01;
+import robocode.*;
+//import java.awt.Color;
+
+// Estratégia sugerida:
+// strafing 
+// movimento 
+// perpendicular 
+// wall smoothing
+// random movement
+ 
+
