5d69d3cf09ef7bdeadc2e4df434ee3d9124396f9
kaio <kaio@ifsc>
2026-05-22 20:50:59 -0300

    Commit da V1 do SS. Melhorias na movimentação, tiro, defesa e radar. Organização do diretório - robo agora ficará no diretório /kaiorobo

--- robokaio/SilvioSantos.java
+++ robokaio/SilvioSantos.java
@@ -1,6 +1,6 @@
 package kr;
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
@@ -18,39 +18,65 @@ public class SilvioSantos extends Robot
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		setColors(Color.black,Color.red,Color.white); // body,gun,radar
 
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
+			
+			// robo anda para frente
+			ahead(150);
+
+			// robo para a esquerda
+			turnRight(90);
+
+			// robo gira canhao procurando inimigos
 			turnGunRight(360);
+
+			// robo anda para tras
 			back(100);
-			turnGunRight(360);
+
+			// robo vira para a esquerda
+			turnLeft(45);
+			
 		}
 	}
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
+	
+	// 
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
+		
+		// dispara um tiro 1 para economizar energia
 		fire(1);
+		
+		// gira um pouco o canhão
+		turnGunRight(15);
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+		
+		// Evasão - anda para trás
+		back(50);
+		
+		// Vira um pouco para mudar de direção
+		turnRight(45);
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
+
+		// Recura para sair da parede
+		back(80);
+		
+		// Vira para tentar encontrar outro caminho
+		turnRight(90);
 	}	
 }
