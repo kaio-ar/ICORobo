d0c1b580f8017d130a84c5694f935a94cd6fa6e6
Alan <alan@ifsc>
2026-05-23 21:23:05 -0300

    Atualizando o robo 1.0

--- robokaio/SilvioSantos.java
+++ robokaio/SilvioSantos.java
@@ -2,81 +2,52 @@ package kr;
 import robocode.*;
 import java.awt.Color;
 
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
-/**
- * SilvioSantos - a robot by (your name here)
- */
-public class SilvioSantos extends Robot
-{
-	/**
-	 * run: SilvioSantos's default behavior
-	 */
-	public void run() {
-		// Initialization of the robot should be put here
+public class SilvioSantos extends Robot {
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
+    public void run() {
 
-		setColors(Color.black,Color.red,Color.white); // body,gun,radar
+        // CORES DO ROBO
+        setColors(Color.black, Color.red, Color.white);
 
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			
-			// robo anda para frente
-			ahead(150);
+        // LOOP PRINCIPAL, EXECUTA OS COMANDO ABAIXO SEM PARAR
+        while (true) {
 
-			// robo para a esquerda
-			turnRight(90);
+            // Anda para frente
+            ahead(100);
 
-			// robo gira canhao procurando inimigos
-			turnGunRight(360);
+            // Vira um pouco
+            turnRight(90);
 
-			// robo anda para tras
-			back(100);
+            // Procura inimigos
+            turnGunRight(360);
+        }
+    }
 
-			// robo vira para a esquerda
-			turnLeft(45);
-			
-		}
-	}
+   // QUANDO ENCONTRAR UM INIMIGO
+   
+    public void onScannedRobot(ScannedRobotEvent e) {
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
+        fire(1);
+    }
+
+    //QUANDO BATE NA PAREDE
 	
-	// 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		
-		// dispara um tiro 1 para economizar energia
-		fire(1);
-		
-		// gira um pouco o canhão
-		turnGunRight(15);
-	}
+    public void onHitWall(HitWallEvent e) {
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		
-		// Evasão - anda para trás
-		back(50);
-		
-		// Vira um pouco para mudar de direção
-		turnRight(45);
-	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
+        
+        back(50);
 
-		// Recura para sair da parede
-		back(80);
-		
-		// Vira para tentar encontrar outro caminho
-		turnRight(90);
-	}	
+        turnRight(90);
+    }
+
+    // QUANDO FOR ATINGIDO
+    public void onHitByBullet(HitByBulletEvent e) {
+
+        
+        ahead(50);
+
+
+        turnLeft(45);
+    }
 }
